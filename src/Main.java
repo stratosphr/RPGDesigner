35dda95d1f0797d4117a02f0ac9079af6740105b
stratosphr <stratosphr@gmail.com>
2018-07-20 16:22:05 +0200

    Changed table view to tree table view for displaying previewed node properties Quit item closes the main window Simplified Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import controllers.MasterController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * Created by stratosphr on 20/07/2018.
  */
-public final class Main {
+public final class Main extends Application {
 
-    public static void main(String[] args) {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/main-window.fxml"));
+        loader.setController(MasterController.getSingleton());
+        primaryStage.setTitle("RPGDesigner");
+        primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.show();
     }
 
 }
