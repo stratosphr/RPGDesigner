b3274617090b64bb7585a26fcd4c0b9accb62fbe
stratosphr <stratosphr@gmail.com>
2018-07-21 00:47:20 +0200

    Fixed null pointer exception occurring when showing the main window Added basic custom cell factory to nodes hierarchy tree view

--- src/controllers/NodesHierarchyController.java
+++ src/controllers/NodesHierarchyController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import models.MasterModel;
@@ -21,6 +23,26 @@ public final class NodesHierarchyController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        tree_nodesHierarchy.setCellFactory(param -> new TreeCell<>() {
+            @Override
+            public void startEdit() {
+                super.startEdit();
+                setText(null);
+                setGraphic(new TextField("test"));
+            }
+
+            @Override
+            protected void updateItem(ANode item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    setText(item.nameProperty().get());
+                    setGraphic(null);
+                }
+            }
+        });
         tree_nodesHierarchy.setRoot(new TreeItem<>(new ANode("Project") {
         }));
         tree_nodesHierarchy.getRoot().getChildren().add(new TreeItem<>(new ANode("node1", new ANodeProperty<>("property1", 42) {
