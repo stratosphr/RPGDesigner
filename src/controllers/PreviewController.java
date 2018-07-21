969112de4cdc5a78674e2c249224b084ccf763d4
stratosphr <stratosphr@gmail.com>
2018-07-21 15:40:56 +0200

    Preview is now refreshed when the previewed node or one of its properties changes

--- src/controllers/PreviewController.java
+++ src/controllers/PreviewController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import models.MasterModel;
+import models.nodes.ANode;
 
 /**
  * Created by stratosphr on 20/07/2018.
@@ -14,9 +15,17 @@ public final class PreviewController {
     private ScrollPane scroll_preview;
 
     public void initModel(MasterModel model) {
+        model.previewedNodeProperty().addListener((observable, oldValue, newValue) -> {
+            previewNode(newValue);
+            newValue.nameProperty().addListener((observable1, oldValue1, newValue1) -> previewNode(newValue));
+            newValue.getProperties().forEach(nodeProperty -> nodeProperty.valueProperty().addListener((observable1, oldValue1, newValue1) -> previewNode(newValue)));
+        });
+    }
+
+    private void previewNode(ANode node) {
         Label label = new Label();
+        label.setText(node.toString());
         scroll_preview.setContent(label);
-        model.previewedNodeProperty().addListener((observable, oldValue, newValue) -> label.textProperty().bind(newValue.nameProperty()));
     }
 
 }
